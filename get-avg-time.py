2a3d30856b173c19743a6eaad00420302778d245
yb <lojobs38@gmail.com>
2022-09-17 01:35:24 +0800

    Update py script

--- get-avg-time.py
+++ get-avg-time.py
@@ -4,6 +4,13 @@ import logging
 import sys
 from numpy import mean
 
+# variables
+testHowManyTimes = 2
+eachTimesArr = []
+totalSumTimes = 0
+filename = "logs/test2.log"
+arr_each_avg_time = []
+
 # logging use
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -13,17 +20,12 @@ stdout_handler = logging.StreamHandler(sys.stdout)
 stdout_handler.setLevel(logging.DEBUG)
 stdout_handler.setFormatter(formatter)
 
-file_handler = logging.FileHandler('logs/logs-test11.log')
+file_handler = logging.FileHandler(filename)
 file_handler.setLevel(logging.DEBUG)
 file_handler.setFormatter(formatter)
 
 logger.addHandler(file_handler)
 logger.addHandler(stdout_handler)
-
-
-testHowManyTimes = 2
-eachTimesArr = []
-totalSumTimes = 0
 
 for i in range(testHowManyTimes):
     subprocess.run(["mvn", "test"])
@@ -32,7 +34,8 @@ for i in range(testHowManyTimes):
     resultTimes = os.popen(cmdOnlyTime).read()
     # logger.info(f"resultTimes: {resultTimes}")
     afterSplitArr = resultTimes.splitlines()
-    eachTimesArr.append(afterSplitArr)
+    float_list = [float(index_list) for index_list in afterSplitArr]
+    eachTimesArr.append(float_list)
     # logger.info(f"afterSplitArr: {afterSplitArr}")
     for j in range(len(afterSplitArr)):
         totalSumTimes = totalSumTimes + float(afterSplitArr[j])
@@ -40,12 +43,11 @@ for i in range(testHowManyTimes):
 logger.info("Total times: %f", totalSumTimes)
 logger.info(f"EachTimesArr: {eachTimesArr}")
 
-arr_each_avg_time = []
 
 for i in range(len(eachTimesArr)):
     each_avg_time = 0
-    float_list = [float(index_list) for index_list in eachTimesArr[i]]
-    each_avg_time = mean(float_list)
+    # float_list = [float(index_list) for index_list in eachTimesArr[i]]
+    each_avg_time = mean(eachTimesArr[i])
     arr_each_avg_time.append(each_avg_time)
 
 logger.info(f"arr_each_avg_time: {arr_each_avg_time}")
