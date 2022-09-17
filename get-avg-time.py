8784e463e94a77ad6a57612c41071e1332229b48
yb <lojobs38@gmail.com>
2022-09-17 13:59:38 +0800

    Update py test script

--- get-avg-time.py
+++ get-avg-time.py
@@ -5,10 +5,10 @@ import sys
 from numpy import mean
 
 # variables
-testHowManyTimes = 2
+testHowManyTimes = 10
 eachTimesArr = []
 totalSumTimes = 0
-filename = "logs/test2.log"
+filename = "logs/pchomeTest-thread8-check10times-1.log"
 arr_each_avg_time = []
 
 # logging use
@@ -28,28 +28,24 @@ logger.addHandler(file_handler)
 logger.addHandler(stdout_handler)
 
 for i in range(testHowManyTimes):
+    logger.info(f"Running test {i+ 1} now.")
+    each_avg_time = 0
     subprocess.run(["mvn", "test"])
 
     cmdOnlyTime = "grep -h testcase target/surefire-reports/TEST-*.xml | awk -F '\"' '{print $6 }' | sort -rn -k 3"
     resultTimes = os.popen(cmdOnlyTime).read()
-    # logger.info(f"resultTimes: {resultTimes}")
     afterSplitArr = resultTimes.splitlines()
     float_list = [float(index_list) for index_list in afterSplitArr]
+    logger.info(f"Time now: {float_list}") #[3.5, 3.437, 3.33, 3.266]
     eachTimesArr.append(float_list)
+    each_avg_time = mean(eachTimesArr[i])
+    logger.info(f"Average time now: {each_avg_time}") # 3.38325
+    arr_each_avg_time.append(each_avg_time)
     # logger.info(f"afterSplitArr: {afterSplitArr}")
     for j in range(len(afterSplitArr)):
         totalSumTimes = totalSumTimes + float(afterSplitArr[j])
 
-logger.info("Total times: %f", totalSumTimes)
-logger.info(f"EachTimesArr: {eachTimesArr}")
-
-
-for i in range(len(eachTimesArr)):
-    each_avg_time = 0
-    # float_list = [float(index_list) for index_list in eachTimesArr[i]]
-    each_avg_time = mean(eachTimesArr[i])
-    arr_each_avg_time.append(each_avg_time)
-
-logger.info(f"arr_each_avg_time: {arr_each_avg_time}")
-
-logger.info("Total mean: %f", round(mean(arr_each_avg_time), 4))
+logger.info(f"Time required for each cost: {arr_each_avg_time}")
+logger.info("It took %f seconds in total", totalSumTimes)
+logger.info(f"Average time required for each cost: {eachTimesArr}")
+logger.info("Total average seconds: %f", round(mean(arr_each_avg_time), 4))
